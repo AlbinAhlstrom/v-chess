f81a8afebbdfcf6b5026bcfc22350143e6cf8776
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-12 14:05:29 +0100

    Remove faulty walrus operator in diagonal moves comprehension

--- pieces/movement.py
+++ pieces/movement.py
@@ -2,25 +2,23 @@ from chess import Coordinate
 from pieces import Piece
 
 
-class StraightMovingPiece(Piece):
-    def moves(self):
-        p = self.position
+def straight_moves(piece: Piece):
+    p = piece.position
 
-        horizontal_moves = {Coordinate(p.row, col) for col in range(0, 8)}
-        vertical_moves = {Coordinate(row, p.col) for row in range(0, 8)}
+    horizontal_moves = {Coordinate(p.row, col) for col in range(0, 8)}
+    vertical_moves = {Coordinate(row, p.col) for row in range(0, 8)}
 
-        return (horizontal_moves | vertical_moves) - {Coordinate(p.row, p.col)}
+    return (horizontal_moves | vertical_moves) - {Coordinate(p.row, p.col)}
 
 
-class DiagonalMovingPiece(Piece):
-    def moves(self):
-        p = self.position
+def diagonal_moves(piece: Piece):
+    p = piece.position
 
-        cols = [col := p.col + i for i in range(-7, 8) if 0 <= col < 8]
-        rows = [row := p.row + i for i in range(-7, 8) if 0 <= row < 8]
-        inv_rows = [row := p.row - i for i in range(-7, 8) if 0 <= row < 8]
+    cols = [p.col + i for i in range(-7, 8) if 0 <= p.col + i < 8]
+    rows = [p.row + i for i in range(-7, 8) if 0 <= p.row + i < 8]
+    inv_rows = [p.row - i for i in range(-7, 8) if 0 <= p.row - i < 8]
 
-        diag = {Coordinate(row, col) for row, col in zip(rows, cols)}
-        inv_diag = {Coordinate(row, col) for row, col in zip(inv_rows, cols)}
+    diag = {Coordinate(row, col) for row, col in zip(rows, cols)}
+    inv_diag = {Coordinate(row, col) for row, col in zip(inv_rows, cols)}
 
-        return (diag | inv_diag) - {Coordinate(p.row, p.col)}
+    return (diag | inv_diag) - {Coordinate(p.row, p.col)}
