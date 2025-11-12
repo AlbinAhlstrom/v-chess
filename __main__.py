2a6f94fa7ea3b70a3e66912b86b3d904d3b2a7c2
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-12 08:20:01 +0100

    Move board initialization to __main__.py

---
+++ __main__.py
@@ -0,0 +1,46 @@
+from pieces import Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook, Pawn
+from board import Color, Coordinate
+
+
+def main():
+    def setup_starting_board():
+        piece_order = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+
+        board = [[None for _ in range(8)] for _ in range(8)]
+
+        board[1] = [Pawn(Color.BLACK, Coordinate(1, col)) for col in range(8)]
+        board[6] = [Pawn(Color.WHITE, Coordinate(6, col)) for col in range(8)]
+
+        board[0] = [
+            piece(Color.BLACK, Coordinate(0, col))
+            for col, piece in enumerate(piece_order)
+        ]
+        board[7] = [
+            piece(Color.WHITE, Coordinate(7, col))
+            for col, piece in enumerate(piece_order)
+        ]
+
+        return board
+
+    board = setup_starting_board()
+    history = [board]
+    player_to_move = Color.WHITE
+    castle_allowed = [Color.WHITE, Color.BLACK]
+    en_passant_square = None
+    halfmove_clock = 0
+
+    state = BoardState(
+        board,
+        history,
+        player_to_move,
+        castle_allowed,
+        en_passant_square,
+        halfmove_clock,
+    )
+    for row in state.board:
+        row = [char or 0 for char in row]
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
