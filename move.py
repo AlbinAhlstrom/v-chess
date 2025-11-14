7f810ea338ebd614ac6cbd595f16194a98466b2b
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-14 08:51:58 +0100

    Refactor Move to dataclass

--- move.py
+++ move.py
@@ -1,12 +1,24 @@
+from dataclasses import dataclass
+
+from chess.board_utils import Coordinate
+from chess.pieces import Piece
+
+
+@dataclass(frozen=True)
 class Move:
-    def __init__(
-        self,
-        start_square,
-        end_square,
-        piece_moved,
-        piece_captured=None,
-        is_en_passant=False,
-        is_castling=False,
-        is_double_pawn_move=False,
-    ):
-        pass
+    start: Coordinate
+    end: Coordinate
+    piece_moved: Piece
+    piece_captured: Piece = None
+    promotion_piece: Piece = None
+    is_en_passant: bool = False
+    is_castling: bool = False
+    is_double_pawn_push: bool = False
+
+    @property
+    def is_promotion(self) -> bool:
+        return self.promotion_piece is not None
+
+    @property
+    def is_capture(self) -> bool:
+        return self.piece_captured is not None or self.is_en_passant
