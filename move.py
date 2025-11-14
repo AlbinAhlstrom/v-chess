210234f2c10ec3c7afa29a32369c7fad9c74e16b
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-14 08:44:26 +0100

    Add move.py

---
+++ move.py
@@ -0,0 +1,12 @@
+class Move:
+    def __init__(
+        self,
+        start_square,
+        end_square,
+        piece_moved,
+        piece_captured=None,
+        is_en_passant=False,
+        is_castling=False,
+        is_double_pawn_move=False,
+    ):
+        pass
