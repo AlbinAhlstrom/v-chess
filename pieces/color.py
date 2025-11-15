dbe1cd84b0e85078aef43cdf7f39c8273df3d9d5
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-15 14:05:22 +0100

    Move color to separate file

---
+++ pieces/color.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class Color(Enum):
+    """Representation of piece/player color.
+
+    Attributes:
+        BLACK: Represents black pieces/player.
+        WHITE: Represents white pieces/player.
+    """
+
+    BLACK = 0
+    WHITE = 1
