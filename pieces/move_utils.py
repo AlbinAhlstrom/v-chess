3e8e4f3f937d48d2c73fba21d3f0c6812344b9f0
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-15 15:16:37 +0100

    Add Direction and Moveset enums

--- pieces/move_utils.py
+++ pieces/move_utils.py
@@ -1,38 +1,41 @@
+from enum import Enum
 from typing import Iterable
 
 from chess.square import Square
 from pieces import Piece
 
 
-def straight_moves(piece: Piece):
-    pos = piece.position
-
-    horizontal_moves = [Square(pos.row, col) for col in range(0, 8)]
-    vertical_moves = [Square(row, pos.col) for row in range(0, 8)]
-
-    return (horizontal_moves | vertical_moves) - {Square(pos.row, pos.col)}
-
-
-def diagonal_moves(piece: Piece):
-    pos = piece.position
-
-    diagonal = {
-        Square(pos.row + offset, pos.col + offset)
-        for offset in range(-7, 8)
-        if 0 <= pos.row + offset < 8 and 0 <= pos.col + offset < 8
-    }
-    inverted_diagonal = {
-        Square(pos.row + offset, pos.col - offset)
-        for offset in range(-7, 8)
-        if 0 <= pos.row + offset < 8 and 0 <= pos.col - offset < 8
-    }
-    return (diagonal | inverted_diagonal) - {Square(pos.row, pos.col)}
+class Direction(Enum):
+    UP = (0, -1)
+    DOWN = (0, 1)
+    LEFT = (-1, 0)
+    RIGHT = (1, 0)
+    UP_LEFT = (-1, -1)
+    UP_RIGHT = (1, -1)
+    DOWN_LEFT = (-1, 1)
+    DOWN_RIGHT = (1, 1)
 
 
-def limit_distance(piece: Piece, moves: Iterable[Square], limit: int) -> set[Square]:
-    return {
-        move
-        for move in moves
-        if abs(piece.position.row - move.row) <= limit
-        and abs(piece.position.col - move.col) <= limit
-    }
+class Moveset(Enum):
+    STRAIGHT = [Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT]
+    DIAGONAL = [
+        Direction.UP_LEFT,
+        Direction.UP_RIGHT,
+        Direction.DOWN_LEFT,
+        Direction.DOWN_RIGHT,
+    ]
+    STRAIGHT_AND_DIAGONAL = STRAIGHT + DIAGONAL
+
+
+def get_line_from(square: Square, direction: Direction):
+    row_increment, col_increment = direction
+    row = square.row + row_increment
+    col = square.col + col_increment
+    line = []
+
+    while 0 <= row < 8 and 0 <= col < 8:
+        line.append(Coordinate(row, col))
+        row += row_increment
+        col += col_increment
+
+    return line
