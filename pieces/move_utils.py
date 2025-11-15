62c1be7d9551cf47134e5c60f2ae65039a18bbab
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-15 14:10:01 +0100

    Rename movement.py to move_utils.py

--- pieces/move_utils.py
+++ pieces/move_utils.py
@@ -1,37 +1,35 @@
 from typing import Iterable
 
-from chess import Coordinate
+from chess.square import Square
 from pieces import Piece
 
 
 def straight_moves(piece: Piece):
     pos = piece.position
 
-    horizontal_moves = {Coordinate(pos.row, col) for col in range(0, 8)}
-    vertical_moves = {Coordinate(row, pos.col) for row in range(0, 8)}
+    horizontal_moves = [Square(pos.row, col) for col in range(0, 8)]
+    vertical_moves = [Square(row, pos.col) for row in range(0, 8)]
 
-    return (horizontal_moves | vertical_moves) - {Coordinate(pos.row, pos.col)}
+    return (horizontal_moves | vertical_moves) - {Square(pos.row, pos.col)}
 
 
 def diagonal_moves(piece: Piece):
     pos = piece.position
 
     diagonal = {
-        Coordinate(pos.row + offset, pos.col + offset)
+        Square(pos.row + offset, pos.col + offset)
         for offset in range(-7, 8)
         if 0 <= pos.row + offset < 8 and 0 <= pos.col + offset < 8
     }
     inverted_diagonal = {
-        Coordinate(pos.row + offset, pos.col - offset)
+        Square(pos.row + offset, pos.col - offset)
         for offset in range(-7, 8)
         if 0 <= pos.row + offset < 8 and 0 <= pos.col - offset < 8
     }
-    return (diagonal | inverted_diagonal) - {Coordinate(pos.row, pos.col)}
+    return (diagonal | inverted_diagonal) - {Square(pos.row, pos.col)}
 
 
-def limit_distance(
-    piece: Piece, moves: Iterable[Coordinate], limit: int
-) -> set[Coordinate]:
+def limit_distance(piece: Piece, moves: Iterable[Square], limit: int) -> set[Square]:
     return {
         move
         for move in moves
