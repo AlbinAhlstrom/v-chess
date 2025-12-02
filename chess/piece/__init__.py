2e6b1e8b5d2184458285fe319266163f28ce3ad7
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-12-02 20:13:45 +0100

    Add all piece subclasses of piece to import through piece.__init__

--- chess/piece/__init__.py
+++ chess/piece/__init__.py
@@ -1,11 +1,12 @@
 from typing import Type
-from .piece import Piece
+from .piece import *
 from .rook import Rook
 from .knight import Knight
 from .bishop import Bishop
 from .queen import Queen
 from .king import King
 from .pawn import Pawn
+
 
 
 piece_from_char: dict[str, Type[Piece]] = {
