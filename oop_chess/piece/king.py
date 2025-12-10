64abde34257a38fc05631a0a82c953294023fcd0
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-12-10 14:46:15 +0100

    Refine king theoretical moves to include castling

--- oop_chess/piece/king.py
+++ oop_chess/piece/king.py
@@ -2,6 +2,7 @@ from math import inf
 
 from oop_chess.piece.piece import Piece
 from oop_chess.enums import Color, Direction
+from oop_chess.square import Square
 
 
 class King(Piece):
@@ -13,7 +14,19 @@ class King(Piece):
 
     moveset = Direction.straight_and_diagonal()
     MAX_STEPS = 1
+    CASTLING_MAX_STEPS = 2
 
+    def _max_steps(self, direction):
+        """Allow moving two squares when castling."""
+        if direction in (Direction.LEFT, Direction.RIGHT) and not self.has_moved:
+            return self.CASTLING_MAX_STEPS
+        else:
+            return self.MAX_STEPS
+
+    @property
+    def theoretical_move_paths(self) -> list[list[Square]]:
+        """Theoretical move paths adjusted to allow for castling."""
+        return [direction.get_path(self.square, self._max_steps(direction)) for direction in self.moveset]
 
     def __str__(self):
         match self.color:
