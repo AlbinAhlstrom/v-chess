374cfe7ddc30f70fdd17d9a506b074c07c4a7663
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-12-27 01:35:32 +0100

    style: replace new opponent icon with plus-circle to avoid confusion

--- frontend/src/components/Pieces/subcomponents/GameSidebar.js
+++ frontend/src/components/Pieces/subcomponents/GameSidebar.js
@@ -57,8 +57,8 @@ const REMATCH_ICON = (
 );
 
 const NEW_OPPONENT_ICON = (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" fill="currentColor" style={{ width: 'var(--button-icon-size)', height: 'var(--button-icon-size)' }}>
-        <path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM504 312V248H440V216H504V152H536V216H600V248H536V312H504z"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" style={{ width: 'var(--button-icon-size)', height: 'var(--button-icon-size)' }}>
+        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM232 344V280H168c-13.3 0-24-10.7-24-24s10.7-24 24-24h64V168c0-13.3 10.7-24 24-24s24 10.7 24 24v64h64c13.3 0 24 10.7 24 24s-10.7 24-24 24H280v64c0 13.3-10.7 24-24 24s-24-10.7-24-24z"/>
     </svg>
 );
 
